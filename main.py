c8ff7beb069d613c77c3b8f63be639575f176540
bendenko-v <bendenko.v@gmail.com>
2023-02-28 19:19:02 +0100

    Add 'key_date' function to sort list by dates

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from utils import load_json
+from utils import load_json, key_date
 
 PATH = 'operations.json'
 
 data = load_json(PATH)
+
+data.sort(key=key_date, reverse=True)  # sorts the list by dates from the latest date
--- utils.py
+++ utils.py
@@ -1,15 +1,32 @@
 import json
+from datetime import datetime
 
 
-def load_json(path):
+def load_json(path: str) -> list[dict]:
     """
-    Load data from JSON
+    Loads data from JSON
 
-    :param path: path to JSON-file with transactions
-    :return: list of dicts (one dict - one transaction)
-    """
+    :param path:
+        path to JSON-file with transactions
+    :return:
+        list of dicts (one dict - one transaction)
+        """
     with open(path, 'r', encoding='utf-8') as file:
         data = json.load(file)
         return data
 
 
+def key_date(transaction: dict) -> datetime.date:
+    """
+    Gets a date in string format from a transaction and return a datetime object
+
+    :param transaction:
+        dict with transaction data
+    :return:
+        datetime.date object with date of transaction
+        if no key 'date' in dict return date '0001-01-01'
+    """
+    if 'date' in transaction:
+        datetime_str = transaction['date'].split('T')[0]
+        return datetime.strptime(datetime_str, '%Y-%m-%d')
+    return datetime.fromisoformat('0001-01-01')
